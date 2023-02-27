8dbf75c4112d8edcb2977cd26c7ffa540b15ac28
ionut diaconu <ionut.diaconu@gmail.com>
2023-02-27 23:16:43 +0200

    test CI/CD

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello to Family TODO (MOD)!</h1>";
+  greeting = "<h1>Hello to Family TODO (MOd2)!</h1>";
   name = req.params["name"];
   if (name) {
     res.send(greeting + "</br>and hello to " + name);
